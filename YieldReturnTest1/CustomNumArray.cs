bd4c2e20c2294e1bf9fc1c5c182288ab8c0432ce
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-11-14 20:59:15 +0400

    Add project files.

---
+++ YieldReturnTest1/CustomNumArray.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldReturnTest1
+{
+	internal class CustomNumArray : IEnumerable<int>
+	{
+		private int[] array;
+
+
+		public CustomNumArray(int[] array)
+		{
+			this.array = array;
+		}
+
+		public CustomNumArray()
+		{
+			this.array = new int[0];
+		}
+
+		public void Add(int num)
+		{
+			int[] newArray = new int[array.Length + 1];
+			for (int i = 0; i < array.Length; i++)
+			{
+				newArray[i] = array[i];
+			}
+			newArray[array.Length] = num;
+			array = newArray;
+		}
+
+		public IEnumerator<int> GetEnumerator()
+		{
+			return new CustomNumArrayEnumerator(this);
+
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public int this[int index]
+		{
+			get
+			{
+				return this.array[index];
+			}
+		}
+
+		private class CustomNumArrayEnumerator : IEnumerator<int>
+		{
+			private readonly CustomNumArray numArray;
+			public CustomNumArrayEnumerator(CustomNumArray numArray)
+			{
+				this.numArray = numArray;
+			}
+
+			#region Enumerator
+
+			int currentIndex = -1;
+			public int Current
+			{
+				get
+				{
+					return numArray.array[currentIndex];
+				}
+			}
+
+			public bool MoveNext()
+			{
+				return ++currentIndex < numArray.array.Length;
+			}
+
+			public void Reset()
+			{
+				currentIndex = -1;
+			}
+
+			object IEnumerator.Current
+			{
+				get
+				{
+					return this.Current;
+				}
+			}
+			public void Dispose()
+			{
+
+			}
+
+			#endregion
+
+		}
+	}
+}
---
+++ YieldReturnTest1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace YieldReturnTest1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CustomNumArray customNumArray = new CustomNumArray();
+			customNumArray.Add(1);
+			customNumArray.Add(2);
+			customNumArray.Add(3);
+			customNumArray.Add(4);
+
+
+			foreach (int num in customNumArray)
+			{
+				Console.WriteLine(num);
+			}
+		}
+	}
+}
